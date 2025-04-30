1618b712978f95a9d1f3f5cdb12c3a03b37b591e
raymondjoseph02 <amraymondjoseph@gmail.com>
2025-04-30 11:05:38 +0100

    feature:implement verify email page ui and fix the closes task isues

--- src/common/auth/AuthFormHeader.tsx
+++ src/common/auth/AuthFormHeader.tsx
@@ -6,7 +6,7 @@ export const AuthFormHeader: FC<AuthFormHeaderProps> = ({
 }) => {
   return (
     <article className="space-y-2">
-      <h2 className="text-[2.37rem] font-bold text-gray-600 dark:text-gray-150 ">
+      <h2 className="text-[2.39rem] font-bold text-gray-600 dark:text-gray-150 ">
         {title}
       </h2>
       <p className="text-xs font-medium text-gray-400 sm:text-base in-dark:text-gray-200">
