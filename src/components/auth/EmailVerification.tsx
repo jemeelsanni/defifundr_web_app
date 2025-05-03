47c59e82e01921cc2e6e66dcf239f29c3195e233
Lazarus <alvarolazarus@gmail.com>
2025-05-02 19:56:20 -0600

    fix: reused the existing global modal

--- src/components/auth/EmailVerification.tsx
+++ src/components/auth/EmailVerification.tsx
@@ -1,25 +1,32 @@
-import React, { useState } from "react";
-import EmailReminderModal from "../modal/EmailReminderModal";
+import React from "react";
+import { useDispatch } from "react-redux";
+import { openModal } from "../../redux/slice/modalSlice";
+import EmailReminderContent from "../modal/EmailReminderContent";
 
 const EmailVerification: React.FC = () => {
-  const [isModalOpen, setIsModalOpen] = useState(false);
+  const dispatch = useDispatch();
 
   const openEmailReminderModal = () => {
-    setIsModalOpen(true);
+    dispatch(
+      openModal({
+        customComponent: <EmailReminderContent />,
+        confirmText: "Close",
+        size: "md",
+        type: "success",
+        onConfirm: () => {
+          // Any additional action needed on close
+        }
+      })
+    );
   };
 
   return (
-    <>
-      <button
-        onClick={openEmailReminderModal}
-        className="text-sm font-bold transition duration-300 ease-in-out text-primary-200 dark:text-primary-400 dark:hover:text-primary-400/70 hover:text-primary-200/70"
-      >
-        Didn't get the code?
-      </button>
-      
-      {/* Render the modal directly */}
-      {isModalOpen && <EmailReminderModal onClose={() => setIsModalOpen(false)} />}
-    </>
+    <button
+      onClick={openEmailReminderModal}
+      className="text-sm font-bold transition duration-300 ease-in-out text-primary-200 dark:text-primary-400 dark:hover:text-primary-400/70 hover:text-primary-200/70"
+    >
+      Didn't get the code?
+    </button>
   );
 };
 
